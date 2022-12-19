276b4eaf3d8a4aa1ad5974a3591c06bef60a94ed
Robkso@gmail.com <Robkso@gmail.com>
2022-12-19 10:53:21 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "tv_show_suggestor_37902",
   url: "https://tv_show_suggestor_37902.botics.co",
   api: "https://tv_show_suggestor_37902.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
